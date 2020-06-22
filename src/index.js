f57bd3465de2e8a0340e8e830536db4b1dedc4d7
Prarabdh Garg <prarabdhgrg@gmail.com>
2020-06-22 12:08:43 +0530

    Add messaging module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const  thinQ = require('./core/index')
 const cryptography = require('./util/cryptography')
+const messaging = require('./core/messages')
 
 module.exports = {
     thinQ: thinQ,
-    cryptography: cryptography
+    cryptography: cryptography,
+    messaging: messaging
 }
