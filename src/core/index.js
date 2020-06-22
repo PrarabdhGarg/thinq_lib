0bdfcd7ace3107d72df4a1cd8a62d780edf41ce6
coderjedi <himanshu31099@gmail.com>
2020-06-22 11:59:47 +0530

    Filebook

--- src/core/index.js
+++ src/core/index.js
@@ -96,11 +96,6 @@ async function updateInfo(updates , args , ipfs="self") {
 
                 global.User.update({new_info}, {where: {ipfs: ipfs}}).then((result1) => {
                     console.log('Database updated sucessfully')
-                    // message.broadcastMessageToAddressBook({
-                    //     sender: ipfs,
-                    //     action: MessageAction.UPDATE,
-                    //     message: stat.hash.toString(),
-                    // })
                 })
             })
         })
@@ -115,8 +110,40 @@ async function getUsers(){
     return users
 }
 
+// Uploads a file parsed using FileReader to ipfs,adds it to filebook and returns the hash of newly uploaded file
+async function uploadfile(file,documentPath,filename,args){
+    args.node.id().then(async (info)=>{  
+        let hash
+        args.node.files.write(documentPath, Buffer.from(file), {
+                create: true,
+                parents: true
+            }, (err, resp) => {
+                if(err) {
+                    console.log("Error in inserting file " + err.message)
+                } else {
+                    args.node.files.stat(documentPath, (err, respon) => {
+                        if(err) {
+                        }
+                        hash = respon.hash
+                        console.log('Hash of uploaded file is = ' + hash)
+                        args.db.FileBook.findOne({ where: { 'ipfs_hash':hash.toString()} }).then(async (token) => {
+                            if(token === null)
+                            {
+                            args.db.FileBook.create({ipfs_hash: hash.toString(),name:filename})
+                            return hash.toString()
+                            }
+                         })
+                        })
+                        
+                    }
+                })
+})
+}
 
-
-
-
-
+// Returns the filebook of a user
+async function getFilebook(){
+    let files = await args.db.FileBook.findAll({})
+    for(let i=0 ; i<files.length ; i++)
+        files[i] = files[i].dataValues
+    return files
+}
