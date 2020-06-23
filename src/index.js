ff38070bc4fc0d5a1a488e9e4fdba5a21512fbb6
Prarabdh Garg <prarabdhgrg@gmail.com>
2020-06-23 18:48:20 +0530

    Expose filebook api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const  thinQ = require('./core/index')
 const cryptography = require('./util/cryptography')
 const messaging = require('./core/messages')
+const filebook = require('./core/filebook')
 
 module.exports = {
     thinQ: thinQ,
     cryptography: cryptography,
-    messaging: messaging
+    messaging: messaging,
+    filebook: filebook,
 }
