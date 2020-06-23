cae223c4a2d2ab39e2483030ec6632595ddb8683
coderjedi <himanshu31099@gmail.com>
2020-06-23 23:15:20 +0530

    pendingRequestsremoval

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const  thinQ = require('./core/index')
 const cryptography = require('./util/cryptography')
 const messaging = require('./core/messages')
 const filebook = require('./core/filebook')
-const serviceRequest = require('./core/serviceRequests')
+const serviceRequest = require('./core/serviceRequest')
 
 module.exports = {
     thinQ: thinQ,
